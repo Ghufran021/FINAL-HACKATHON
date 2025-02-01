469e42d722e2ae172e09052caea6226c1ba91c3b
Ghufran021 <160762307+Ghufran021@users.noreply.github.com>
2025-02-01 13:18:47 +0500

    commit

--- it/src/app/layout.tsx
+++ it/src/app/layout.tsx
@@ -1,10 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
-import TopHeader from "./Components/TopHeader";
-
-
-const inter = Inter({ subsets: ["latin"] });
+import Footer from "./Components/Footer/page";
+  
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}> <TopHeader/> {children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
